7fbfbc2ec83e63bac694ab67bfb82f1faaf9fcd4
Maaz <devmaaaaz@gmail.com>
2023-05-20 00:02:35 +0500

    Restructured Repo and added Projects page

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// const navLinks = document.querySelectorAll(".navbar a");
+
+// function updateActiveLink() {
+//   navLinks.forEach((link) => {
+//     if (link.getAttribute("href") === window.location.hash) {
+//       link.classList.add("active");
+//     } else {
+//       link.classList.remove("active");
+//     }
+//   });
+// }
+
+// window.onhashchange = updateActiveLink;
+
+// // Initial call to updateActiveLink to set the initial active class on page load
+// updateActiveLink();
